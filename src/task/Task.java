65c6745f530740f1b4fca74b2dc4c02eeb547c59
Daniil VEDISHCHEV <kodalir@Air-de-Daniil.etu.interne.eseo.fr>
2026-03-18 15:32:33 +0100

    upd: new attributes added

--- src/task/Task.java
+++ src/task/Task.java
@@ -1,9 +1,15 @@
 package task;
 import task.domain.*;
+import java.util.Vector;
 public class Task {
     private
         PriorityLevel priorityLevel;
         TaskStatus taskStatus;   
         TaskCategory taskCategory;
         NotificationType notificationType;
+        Vector<Task> HistoryTask;
+
+    public void main(){
+        HistoryTask = new Vector<>();
+    }
 }
