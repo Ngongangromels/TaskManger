9706a081d881279939bd1cc9a0c3721f68b1bf95
Daniil VEDISHCHEV <kodalir@Air-de-Daniil.etu.interne.eseo.fr>
2026-03-18 16:24:20 +0100

    upd: task modification

--- src/task/Task.java
+++ src/task/Task.java
@@ -1,15 +1,30 @@
 package task;
 import task.domain.*;
 import java.util.Vector;
+import java.util.UUID;
 public class Task {
     private
+        String ID;
+        String title;
+
         PriorityLevel priorityLevel;
         TaskStatus taskStatus;   
         TaskCategory taskCategory;
         NotificationType notificationType;
-        Vector<Task> HistoryTask;
+        Vector<Task> dependencies;
+        Vector<HistoryEntry> historyTask;
 
-    public void main(){
-        HistoryTask = new Vector<>();
+    public void main(String taskTitle){
+
+        ID = UUID.randomUUID().toString();
+        title = taskTitle;
+
+        historyTask = new Vector<>();
+        dependencies = new Vector<>();
+
+    }
+
+    public void addHistoryEntry(HistoryEntry entry){
+        historyTask.add(entry);
     }
 }
