4097df41d7a9759a01eb282dfe9fc46b8e694b12
Daniil VEDISHCHEV <kodalir@Air-de-Daniil.etu.interne.eseo.fr>
2026-03-18 16:08:17 +0100

    upd: added HistoryEntry

---
+++ src/task/domain/HistoryEntry.java
@@ -0,0 +1,34 @@
+package task.domain;
+import java.time.LocalDateTime;
+import java.lang.String;
+
+public class HistoryEntry {
+    private
+    LocalDateTime timestamp;
+    String createdBy;
+    String action;
+
+    public void HistoryEntry(String actionMade,String by){
+        action = actionMade;
+        createdBy = by;
+        timestamp = LocalDateTime.now();
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public String getPerformedBy() {
+        return createdBy;
+    }
+
+    @Override
+    public String toString(){
+        return "[ "+timestamp+" ]" + " | " + action + " | " + " by " + createdBy + "\n";
+    }
+
+}
