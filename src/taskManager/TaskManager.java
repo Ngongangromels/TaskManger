709eb3ebcfa17f6346ddabf0f1f82887d49a6cb4
Daniil VEDISHCHEV <kodalir@Air-de-Daniil.etu.interne.eseo.fr>
2026-03-19 12:06:20 +0100

    upd: taskManager

---
+++ src/taskManager/TaskManager.java
@@ -0,0 +1,23 @@
+package taskManager;
+import user.*;
+import task.*;
+
+import java.util.HashMap;
+
+public class TaskManager {
+
+    private HashMap<String,Task> tasks = new HashMap<>();
+
+    public Task createTask(User usr,String taskTitle){
+        if (usr.getRole() == Role.Admin){
+            Task task = new Task(taskTitle);
+            tasks.put(task.ID, task);
+            return task;
+        } else throw new RuntimeException("You have no permission to create tasks!\n");
+    }
+
+    public void deleteTask(User usr, Task task){
+        if (usr.getRole() == Role.Admin)tasks.remove(task.ID);
+        else throw new RuntimeException("You have no permission to delete tasks!\n");
+    }
+}
