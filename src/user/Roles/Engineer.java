57f092a6d3e6d61c33846f4077d953f44f8cc45b
Daniil VEDISHCHEV <kodalir@Air-de-Daniil.etu.interne.eseo.fr>
2026-03-20 11:42:00 +0100

    upd: user

--- src/user/Roles/Engineer.java
+++ src/user/Roles/Engineer.java
@@ -1,9 +1,21 @@
 package user.Roles;
 
 import user.*;
+import task.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class Engineer extends User {
+
+    private List<Task> assignedTasks;
+
     public Engineer(String usrName){
         super(usrName,Role.Engineer);
+        this.assignedTasks = new ArrayList<>();
+    }
+
+    public List<Task> getTasks(){
+        return this.assignedTasks;
     }
 }
